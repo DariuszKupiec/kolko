5ee15eba8a564cc498327a24082907dcb77f06e3
DariuszKupiec <dariusz.kupiec@wsb.bydgosz.pl>
2021-12-18 10:30:40 +0100

    skończona plansza do gry

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 #%%
-
 # tworzę grę kółko-krzyżyk
 # tworzymy słownik który jest naszą planszą do gry - tworzymy słownik PlanszaDoGry
 
-
 PlanszaDoGry = {'7':' ','8':' ','9':' ',
                 '4':' ','5':' ','6':' ',
-                '1':' ','2':' ','3':' ',}
+                '1':' ','2':' ','3':' '}
 
+klawiszeGry=[]
+
+for key in PlanszaDoGry:
+    klawiszeGry.append(key)
+
+print(klawiszeGry)
+
+def drukujPlansze(pole):
+    print(f"{pole['7']}|{pole['8']}|{pole['9']}")
+    print('-+-+-')
+    print(f"{pole['4']}|{pole['5']}|{pole['6']}")
+    print('-+-+-')
+    print(f"{pole['1']}|{pole['2']}|{pole['3']}")
+
+
+drukujPlansze(PlanszaDoGry)
+
+# %%
